274397a1728efa74c2911dabc3cbd086279640ff
GorosX <gorosking7@gmail.com>
2024-11-14 11:31:30 +0100

    AutoPostfix.sh

--- AutoPostfix.sh
+++ AutoPostfix.sh
@@ -70,11 +70,10 @@ apt install -y mailutils
 # Preguntar al usuario por la dirección de correo, CC y el asunto
 
 read -p "Introduce la dirección de correo a la que quieres enviar el mensaje: " EMAIL
-read -p "Introduce la dirección de correo para CC: " CC_EMAIL
 read -p "Introduce el asunto del mensaje: " ASUNTO
 
 # Enviar el correo electrónico
 
-echo "Correo electrónico de prueba" | mail -s "$ASUNTO" -c "$CC_EMAIL" "$EMAIL"
+echo "Correo electrónico de prueba" | mail -s "$ASUNTO" "$EMAIL"
 
-echo "Se ha enviado un mensaje de confirmación a $EMAIL, con CC a $CC_EMAIL."
+echo "Se ha enviado un mensaje de confirmación a $EMAIL."
